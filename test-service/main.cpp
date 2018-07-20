92e15b04fcc19b3d49f08057c6facbdb120e8569
jwilcox <justin.wilcox@adtran.com>
2018-07-20 15:48:19 -0500

    Record module change events

--- test-service/main.cpp
+++ test-service/main.cpp
@@ -1,6 +1,10 @@
 #include <sysrepo.h>
+#include <sysrepo/values.h>
 #include <unistd.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 #define SR_TRY(x)                                                               \
     do {                                                                        \
@@ -10,6 +14,39 @@
             return rc;                                                          \
         }                                                                       \
     } while(false)
+
+namespace std
+{
+std::string to_string(sr_notif_event_t e)
+{
+    switch(e)
+    {
+    case SR_EV_VERIFY:
+        return "SR_EV_VERIFY";
+    case SR_EV_APPLY:
+        return "SR_EV_APPLY";
+    case SR_EV_ABORT:
+        return "SR_EV_ABORT";
+    case SR_EV_ENABLED:
+        return "SR_EV_ENABLED";
+    }
+}
+
+std::string to_string(sr_change_oper_t c)
+{
+    switch(c)
+    {
+    case SR_OP_CREATED:
+        return "SR_OP_CREATED";
+    case SR_OP_MODIFIED:
+        return "SR_OP_MODIFIED";
+    case SR_OP_DELETED:
+        return "SR_OP_DELETED";
+    case SR_OP_MOVED:
+        return "SR_OP_MOVED";
+    }
+}
+}
 
 class SysrepoListener
 {
@@ -72,6 +109,54 @@ private:
                                 sr_notif_event_t event,
                                 void *data)
     {
+        return ((SysrepoListener*)data)->handleChanges(session, module, event);
+    }
+
+    int handleChanges(sr_session_ctx_t *session,
+                      const char *module,
+                      sr_notif_event_t event)
+    {
+        std::ofstream events("/tmp/test-service-event-stream.yml",
+                             std::ios::out | std::ios::app | std::ios::ate);
+        if(!events)
+        {
+            std::cerr << "Failed to open event log file\n";
+            return SR_ERR_OPERATION_FAILED;
+        }
+        std::ostringstream selector;
+        selector << "/" << module << ":*";
+        sr_change_iter_t *iter;
+        SR_TRY(sr_get_changes_iter(session, selector.str().c_str(), &iter));
+
+        events << "---\n";
+        events << "event_type: " << std::to_string(event) << "\n";
+        events << "values:\n";
+
+        sr_val_t *old;
+        sr_val_t *new_;
+        sr_change_oper_t op;
+        while(sr_get_change_next(session, iter, &op, &old, &new_) == SR_ERR_OK)
+        {
+            events << "  - operation: " << std::to_string(op) << "\n";
+            if(old)
+            {
+                events << "    old-path: " << old->xpath << "\n";
+                char *v = sr_val_to_str(old);
+                events << "    old-value: " << v << "\n";
+                free(v);
+            }
+
+            if(new_)
+            {
+                events << "    new-path: " << new_->xpath << "\n";
+                char *v = sr_val_to_str(new_);
+                events << "    new-value: " << v << "\n";
+                free(v);
+            }
+        }
+
+        sr_free_change_iter(iter);
+
         return SR_ERR_OK;
     }
 
