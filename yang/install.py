971049143b6b94ebd82607d1d3e609a2f2a6f752
jwilcox <justin.wilcox@adtran.com>
2018-07-23 11:23:39 -0500

    Add support for enabling features

--- yang/install.py
+++ yang/install.py
@@ -3,12 +3,18 @@ import subprocess
 import sys
 
 
-with open('manifest.json', 'r') as f:
-    manifest = json.load(f)
-
-for model in manifest['models']:
-    cmd = ['sysrepoctl', '--install', '--yang', model]
+def run(cmd):
     print(' + {}'.format(' '.join(cmd)))
     sys.stdout.flush()
     subprocess.check_call(cmd)
 
+
+with open('manifest.json', 'r') as f:
+    manifest = json.load(f)
+
+for model in manifest['models']:
+    run(['sysrepoctl', '--install', '--yang', model])
+
+for feature in manifest['features']:
+    parts = feature.split(':')
+    run(['sysrepoctl', '--feature-enable', parts[1], '--module', parts[0]])
